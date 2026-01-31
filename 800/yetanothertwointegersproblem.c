dcaae3784369b887d3a04e5464521604ed6892a9
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-01-31 17:30:40 +0500

    code

---
+++ 800/yetanothertwointegersproblem.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+// Function Declaration
+int solution(const int a,const int b);
+
+// Main Function
+int main()
+{
+    int cases;
+    scanf("%d",&cases);
+    int ans[cases];
+    int a,b;
+    for(int i=0;i<cases;i++)
+    {
+        scanf("%d%d",&a,&b);
+        ans[i]=solution(a,b);
+    }
+    for(int i=0;i<cases;i++)
+        printf("%d\n",ans[i]);
+    return 0;
+}
+
+// Function Definition
+int solution(const int a,const int b)
+{
+    int moves=0;
+    moves=(a-b);
+    if(moves<0) moves*=(-1);
+    moves=moves/10;
+    if((a-b)%10!=0) moves++;
+    return moves;
+}
