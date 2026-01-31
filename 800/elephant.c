55de45c976eb3ef1e7951fdca1b4b04300814328
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-01-31 15:33:33 +0500

    code

--- 800/elephant.c
+++ 800/elephant.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int x;
+    int x,steps;
     scanf("%d",&x);
     if(x>=1&&x<=1000000)
     {
-        int steps=x/5;
+        steps=x/5;
         if(x%5!=0)
             steps++;
         printf("%d",steps);
