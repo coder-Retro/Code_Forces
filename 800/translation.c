88766647c9bc037c556820a4dec407d3bb82a166
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-01-31 15:41:14 +0500

    code

--- 800/translation.c
+++ 800/translation.c
@@ -10,9 +10,9 @@ int main()
     int i=1;
     for(a=&word[0],b=&word[strlen(word)-1];a<b;a++,b--,i++)
     {
-        *a += *b;
-        *b = *a - *b;
-        *a -= *b;
+        *a ^= *b;
+        *b = *a ^ *b;
+        *a ^= *b;
     }
     printf("%s",(strcmp(attempt,word)==0)?"YES":"NO");
     return 0;
