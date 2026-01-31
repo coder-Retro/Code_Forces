2b009338138b75b2b9425f871a164ba3db4015e4
mhasnainqadri <hasnainqadri9c@gmail.com>
2026-01-31 15:54:23 +0500

    code

---
+++ 800/codeforceschecking.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdbool.h>
+
+// Function Declaration
+bool solution(const char *test);
+
+// Main Function
+int main()
+{
+    int cases;
+    scanf("%d",&cases);
+    bool ans[cases];
+    char current;
+    for(int i=0;i<cases;i++)
+    {
+        scanf(" %c",&current);
+        ans[i]=solution(&current);
+    }
+    for(int i=0;i<cases;i++)
+        printf("%s\n",(ans[i]==true)?"YES":"NO");
+    return 0;
+}
+
+// Function Definition
+bool solution(const char *test)
+{
+    char arr[]="codeforces";
+    for(int i=0;i<10;i++)
+    {
+        iftest==arr[i])
+            return true;
+    }
+    return false;
+}
